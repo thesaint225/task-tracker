f68091dcfc03b4fcbb790b0e36c579de92c1d681
henrymiessan <miessanhenri@gmail.com>
2025-07-30 08:35:36 +0000

    accept  input from the user

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,3 +1,20 @@
+import { useState } from 'react';
+
 export default function TaskList() {
-  return <div>this is the the task list</div>;
+  const [task, setTask] = useState(''); //create a state variable for input
+  function handleInputChange(event: React.ChangeEvent<HTMLInputElement>) {
+    setTask(event.target.value);
+    console.log(event.target.value);
+    console.log(event.target);
+  }
+  return (
+    <div>
+      <input
+        type='text'
+        value={task}
+        onChange={handleInputChange}
+        placeholder='Enter a task '
+      />
+    </div>
+  );
 }
