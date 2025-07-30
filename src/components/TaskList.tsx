f163ca24faa11ef6ba4bfee3534fcb808ac7cfd6
henrymiessan <miessanhenri@gmail.com>
2025-07-30 09:22:20 +0000

    feat(task): handle adding input to task list on button click

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -2,10 +2,16 @@ import { useState } from 'react';
 
 export default function TaskList() {
   const [task, setTask] = useState(''); //create a state variable for input
+  const [tasks, setTasks] = useState<string[]>([]); //to store the individual task from the user
   function handleInputChange(event: React.ChangeEvent<HTMLInputElement>) {
     setTask(event.target.value);
     console.log(event.target.value);
-    console.log(event.target);
+  }
+
+  function handleAddTask() {
+    if (task.trim() === '') return;
+    setTasks([...tasks, task]);
+    setTask(''); //clear input
   }
   return (
     <div>
@@ -15,6 +21,12 @@ export default function TaskList() {
         onChange={handleInputChange}
         placeholder='Enter a task '
       />
+      <button onClick={handleAddTask}> Add Task</button>
+      <ul>
+        {tasks.map((t, index) => (
+          <li key={index}>{t}</li>
+        ))}
+      </ul>
     </div>
   );
 }
