f0c2418f2e99fd269f7fe19ca7e890a1dbfb03d8
henrymiessan <miessanhenri@gmail.com>
2025-07-30 22:41:46 +0000

    Display the task title

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,8 +1,13 @@
 import { useState } from 'react';
 
+type Task = {
+  title: string;
+  completed: boolean;
+};
+
 export default function TaskList() {
   const [task, setTask] = useState(''); //create a state variable for input
-  const [tasks, setTasks] = useState<string[]>([]); //to store the individual task from the user
+  const [tasks, setTasks] = useState<Task[]>([]); //to store the individual task from the user
   function handleInputChange(event: React.ChangeEvent<HTMLInputElement>) {
     setTask(event.target.value);
     console.log(event.target.value);
@@ -10,7 +15,7 @@ export default function TaskList() {
 
   function handleAddTask() {
     if (task.trim() === '') return;
-    setTasks([...tasks, task]);
+    setTasks([...tasks, { title: task, completed: false }]);
     setTask(''); //clear input
   }
   return (
@@ -24,7 +29,7 @@ export default function TaskList() {
       <button onClick={handleAddTask}> Add Task</button>
       <ul>
         {tasks.map((t, index) => (
-          <li key={index}>{t}</li>
+          <li key={index}>{t.title}</li>
         ))}
       </ul>
     </div>
