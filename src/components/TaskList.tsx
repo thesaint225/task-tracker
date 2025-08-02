4f0329f493c52cfc42270a35d84c94cce851e2d1
henrymiessan <miessanhenri@gmail.com>
2025-08-02 07:15:50 +0000

    add an edit functionality

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -8,16 +8,21 @@ type Task = {
 
 export default function TaskList() {
   const [task, setTask] = useState(''); //create a state variable for input
-  const [tasks, setTasks] = useState<Task[]>([]); //to store the individual task from the user
+  const [tasks, setTasks] = useState<Task[]>(() => {
+    const savedTasks = localStorage.getItem('tasks');
+    try {
+      return savedTasks ? JSON.parse(savedTasks) : [];
+    } catch (error) {}
+  });
   const [filter, setFilter] = useState('all');
+  const [editingId, setEditingId] = useState<number | null>(null); // track which task is being edited
+  const [editTitle, setEditTitle] = useState(''); //store the tempory title
+
+  // save to localStorage whenever tasks change
 
   useEffect(() => {
-    const savedTasks = localStorage.getItem('task');
-    if (savedTasks) {
-      console.log('Loaded from localStorage', savedTasks);
-      setTasks(JSON.parse(savedTasks));
-    }
-  }, []);
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+  }, [tasks]);
 
   function handleInputChange(event: React.ChangeEvent<HTMLInputElement>) {
     setTask(event.target.value);
@@ -47,6 +52,29 @@ export default function TaskList() {
     setTasks((updatedTask) => updatedTask.filter((task) => task.id !== taskId));
   }
 
+  // start editing
+  function handleStarEditing(taskId: number, currentTitle: string) {
+    setEditingId(taskId); //start editing this task
+    setEditTitle(currentTitle); //pre-fill input with current title
+  }
+
+  // update te input
+  function handleEditedTitleChange(event: React.ChangeEvent<HTMLInputElement>) {
+    setEditTitle(event.target.value);
+  }
+
+  // save Edited task
+
+  function handleSaveEdit() {
+    setTasks((prevTasks) =>
+      prevTasks.map((task) =>
+        task.id === editingId ? { ...task, title: editTitle } : task
+      )
+    );
+    setEditingId(null);
+    setEditTitle('');
+  }
+
   const filterTasks = tasks.filter((task) => {
     if (filter === 'active') return !task.completed;
     if (filter == 'completed') return task.completed;
@@ -62,15 +90,39 @@ export default function TaskList() {
         placeholder='Enter a task '
       />
       <button onClick={handleAddTask}> Add Task</button>
+      <p>Loaded tasks: {tasks.length}</p>
       <ul>
         {filterTasks.map((task, _) => (
           <li
             key={task.id}
             style={{ textDecoration: task.completed ? 'line-through' : 'none' }}
           >
-            {task.title}
-            <button onClick={() => handleComplete(task.id)}>complete</button>
-            <button onClick={() => handleDelete(task.id)}>delete</button>
+            {editingId === task.id ? (
+              <>
+                <input
+                  type='text'
+                  value={editTitle}
+                  onChange={handleEditedTitleChange}
+                />
+                <button onClick={handleSaveEdit}>Save</button>
+              </>
+            ) : (
+              <>
+                {task.title}
+                <button onClick={() => handleComplete(task.id)}>
+                  complete
+                </button>
+                <button onClick={() => handleDelete(task.id)}>delete</button>
+                <button
+                  onClick={() => {
+                    setEditingId(task.id);
+                    setEditTitle(task.title);
+                  }}
+                >
+                  Edit
+                </button>
+              </>
+            )}
           </li>
         ))}
       </ul>
